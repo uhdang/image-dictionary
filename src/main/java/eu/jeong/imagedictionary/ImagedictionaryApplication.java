aedc1b094b68cc7fad9f6e3239c6639e4c2801b4
Jung Woo J Jeong <jw@demoup.com>
2020-05-15 01:06:46 +0200

    switch to spring

---
+++ src/main/java/eu/jeong/imagedictionary/ImagedictionaryApplication.java
@@ -0,0 +1,13 @@
+package eu.jeong.imagedictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagedictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagedictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/jeong/imagedictionary/ImagedictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.jeong.imagedictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagedictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
